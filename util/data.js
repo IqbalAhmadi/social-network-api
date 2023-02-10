7ba428bd8d30b3b4e7af2998f0db03bb2c3bda39
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-02-09 17:42:23 -0800

    worked on util data.js file

---
+++ util/data.js
@@ -0,0 +1,64 @@
+const userData = [
+  { username: 'Treighton', email: 'tr8@gmail.com' },
+  { username: 'Michael', email: 'mc@gmail.com' },
+  { username: 'Alex', email: 'alex@gmail.com' },
+  { username: 'Sam', email: 'sam@gmail.com' },
+  { username: 'Iqbal', email: 'iqb@gmail.com' },
+  { username: 'Charlie', email: 'ch88@gmail.com' },
+  { username: 'Alexander', email: 'alexd@gmail.com' },
+  { username: 'Messi', email: 'messi@gmail.com' },
+  { username: 'Ronaldo', email: 'cr7@gmail.com' },
+]
+
+const thoughtData = [
+  {
+    thoughtText:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    thoughtText:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    thoughtText:
+      'adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    thoughtText:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    thoughtText:
+      'amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    thoughtText:
+      'aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    thoughtText:
+      'ipsum lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    thoughtText:
+      'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    thoughtText:
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+]
+
+const reactionData = [
+  { username: 'Treighton', reactionBody: 'outstanding' },
+  { username: 'Michael', reactionBody: 'cool' },
+  { username: 'Alex', reactionBody: 'nice' },
+  { username: 'Sam', reactionBody: 'great shot' },
+  { username: 'Iqbal', reactionBody: 'superb' },
+  { username: 'Charlie', reactionBody: 'hmm' },
+  { username: 'Alexander', reactionBody: 'nteresting' },
+  { username: 'Messi', reactionBody: 'amazing' },
+  { username: 'Ronaldo', reactionBody: 'Nah' },
+]
+
+module.exports = { userData, thoughtData, reactionData }
